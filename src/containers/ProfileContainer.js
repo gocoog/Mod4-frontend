87b1bd018320ecde49c416e64887defe2ccdb483
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-09-29 17:26:07 -0500

    added search

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import ElectionCard from './ElectionCard';
-
+import Search from '../components/Search'
 export default class ProfileContainer extends Component {
     state = {
-        elections: []
+        elections: [],
+        allElections: []
       }
     
       componentDidMount() {
         fetch('http://localhost:3001/elections')
         .then(res => res.json())
         .then(elections => {
-          this.setState({ elections: elections })
+          this.setState({ elections: elections, allElections: elections })
         })
       }
 
+      handleChange = (e) => {
+        this.setState({
+            elections: this.state.allElections.filter(p => p.election_name.toLowerCase().includes(e.target.value.toLowerCase()) )
+          })
+      }
       handleClick = (electionId) => {
         this.state.elections.map(e => {
             let moreVotes = e.number_of_votes + 1
@@ -50,12 +56,15 @@ export default class ProfileContainer extends Component {
       }
     render() {
         return (
+            <>
+            <Search election={this.state.elections} handleChange={this.handleChange} />
             <div className='ui centered cards'>
             {this.state.elections.map(election => 
                 <ElectionCard className='ui card' key={election.id} election={election} handleClick={this.handleClick} />
                 )
                 }
             </div>
+            </>
 
         )
     }
